7f525222b6029de62312c67e6ef86380a16b03a1
Christian Callahan <callahan.0019@gmail.com>
2023-11-16 02:10:09 -0500

    make scroll function relative, change margins for smaller screens

--- components/Content/HeroBanner/ScrollButton.js
+++ components/Content/HeroBanner/ScrollButton.js
@@ -4,31 +4,36 @@ import { useEffect } from "react";
 const ScrollButton = ({ blok }) => {
     useEffect(() => {
         const scrollButton = document.getElementById("scrollButton");
+        const bottomDiv = document.getElementById("bottomDiv")
+        const divStartingTop = bottomDiv.getBoundingClientRect().top;
 
-        const scrollOnClick = () => {
-            window.scrollBy({
-                top: 730,
-                behavior: "smooth",
-            });
-        }
+        const scrollHandler = () => { //Smooth scroll to the bottom of the scroll button element
+            window.scrollTo({top: divStartingTop, behavior: "smooth"});
+            console.log(divStartingTop);
+        };
 
-        scrollButton.addEventListener("click", scrollOnClick);
-    })
+        scrollButton.addEventListener("click", scrollHandler);
+    });
 
     return (
-
-                <div className="h-32 flex flex-col justify-center items-center" {...storyblokEditable(blok)}>
-                    <img
-                        className="w-16 h-16 animate-bounce"
-                        src={blok?.arrow.filename}
-                    />
-                    <button
-                        id="scrollButton"
-                        className="mt-[-12px] text-white text-sm hover:text-base uppercase font-roboto"
-                    >
-                        {blok?.button_text}
-                    </button>
+        <div>
+            <div className="mt-5 mb-16 sm:mb-8 h-fit flex flex-col justify-center items-center" {...storyblokEditable(blok)}>
+                <div className="">
+                <img
+                    className="w-12 animate-bounce"
+                    src={blok?.arrow.filename}
+                />
                 </div>
+                <button
+                    id="scrollButton"
+                    className=" text-white text-sm hover:text-base uppercase font-roboto"
+                >
+                    {blok?.button_text}
+                </button>
+            </div>
+            <div id="bottomDiv"></div>
+        </div>
+
     );
 };
 
