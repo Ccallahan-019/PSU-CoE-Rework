7a8c2e52e885a0bd7ee7405c61f3a9149e770874
Christian Callahan <callahan.0019@gmail.com>
2023-11-15 20:01:12 -0500

    comment on media query hook

--- components/Custom Hooks/MediaQueryHook.js
+++ components/Custom Hooks/MediaQueryHook.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
 
 const useMediaQuery = (query) => {
-    const [matched, setMatched] = useState(false);
+    const [matched, setMatched] = useState(false); //State used to capture media matches value as it changes
 
     useEffect(() => {
-      const mediaMatched = window.matchMedia(query);
+      const mediaMatched = window.matchMedia(query); //Media query
 
-      if (mediaMatched.matches !== matched) {
+      if (mediaMatched.matches !== matched) { //If screen size matches media query, set state to true
         setMatched(mediaMatched.matches);
       }
 
       const mediaHandler = () => {
-        setMatched(mediaMatched.matches)
+        setMatched(mediaMatched.matches); //Event handler to change state if browser size changes to match query
     }
       window.addEventListener("resize", mediaHandler);
 
-      return () => window.removeEventListener("resize", mediaHandler);
+      return () => window.removeEventListener("resize", mediaHandler); //Remove listener on unmount to avoid memory leaks
     });
 
     return matched;
