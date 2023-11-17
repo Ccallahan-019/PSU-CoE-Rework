a1c05ea207f51a75750769958438b2fc6cd65c93
Christian Callahan <callahan.0019@gmail.com>
2023-11-17 00:16:53 -0500

    move embedded iframe into image container

--- components/Content/iEngineer/IEngineerSpotlight.js
+++ components/Content/iEngineer/IEngineerSpotlight.js
@@ -2,11 +2,6 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import { useState } from "react";
 
 const IEngineerSpotlight = ({ blok }) => {
-    const containerShown = "absolute w-full h-[475px] flex justify-center items-center";
-    const backgroundShown = "absolute w-full h-full flex justify-end bg-black opacity-60 transition-[height] duration-[300ms]";
-    const videoShown = "z-[1] max-w-full xs:w-[710px] xs:h-[400px] lg:w-[755px] lg:h-[425px] transition-[height] duration-[700ms]";
-    const closeButtonShown = "flex gap-[6px] items-center h-8 mr-2 md:mr-3 lg:mr-10 mt-1 lg:mt-2 uppercase text-white text-lg font-robotoSlab hover:font-semibold hover:opacity-[0.65]";
-
     const [clicked, isClicked] = useState(false); // State used to show or hide video
     const toggleVideo = () => {
         if (clicked) {
@@ -18,32 +13,34 @@ const IEngineerSpotlight = ({ blok }) => {
 
     return (
         <div>
-            <div className={clicked ? containerShown : "h-0 w-0"}>
-                <div className={clicked ? backgroundShown : "h-0 w-0"}>
-                    <button
-                        onClick={toggleVideo}
-                        className={clicked ? closeButtonShown : "hidden"}
-                    >
-                        <img
-                            className="h-[14px] w-[14px] mt-[1px]"
-                            src={blok?.close_image.filename}
-                        />
-                        Close
-                    </button>
-                </div>
-                <iframe
-                    className={clicked ? videoShown : "h-0 w-0"}
-                    src={blok?.video.filename}
-                    frameBorder="0" allow="fullscreen; picture-in-picture"
-                ></iframe>
-            </div>
             <div className="h-fit flex flex-col lg:grid lg:grid-cols-2 items-center bg-slate-300" {...storyblokEditable(blok)}>
-                <div className="flex items-center justify-self-center">
+                <div className="w-full h-[300px] xxs:h-[425px] xs:h-[530px] flex items-center justify-center bg-slate-500">
                     <img
                         onClick={toggleVideo}
-                        className={"max-h-[475px] cursor-pointer"}
+                        className={clicked ? "h-auto w-0" : "h-auto w-auto cursor-pointer"}
                         src={blok?.spotlight_image.filename}
                     />
+                    <div className={clicked ? "w-full h-full flex flex-col gap-3 justify-center transition-[width] duration-[800ms]" : "w-0 h-full flex flex-col gap-2 justify-center"}>
+                        <button
+                            onClick={toggleVideo}
+                            className="pr-1 md:pr-1.5 w-full flex gap-1 items-center justify-end hover:font-semibold hover:opacity-[0.7]"
+                        >
+                            <img
+                                className="h-[12px] w-[12px] mt-[2px]"
+                                src={blok?.close_image.filename}
+                            />
+                            <p className={clicked ? "text-white uppercase font-robotoSlab" : "hidden" }>
+                                Close
+                            </p>
+                        </button>
+                        <div className="w-full flex justify-center items-center">
+                            <iframe
+                                className="w-full h-[155px] xxs:h-[200px] xs:h-[260px] sm:h-[360px] md:h-[430px] lg:h-[280px] xl:h-[350px] 2xl:h-[425px]"
+                                src="https://player.vimeo.com/video/761896858?h=2a50b9899d&title=0&byline=0"
+                                allow="fullscreen; picture-in-picture">
+                            </iframe>
+                        </div>
+                    </div>
                 </div>
                 <div className="max-h-fit flex flex-col gap-2 lg:gap-5 xl:gap-8 px-6 py-5 md:px-20 lg:pl-5 lg:pr-12 xl:pr-28">
                     <div className="pb-2 flex flex-col gap-1 font-robotoSlab">
