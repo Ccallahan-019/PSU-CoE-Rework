ba6845f11ec8c99eb1d985e4e8a4a3a939a8f826
Christian Callahan <callahan.0019@gmail.com>
2023-11-17 03:46:41 -0500

    add opacity gradient and fluidity to images

--- components/Content/GradUndergrad/GradContent.js
+++ components/Content/GradUndergrad/GradContent.js
@@ -2,11 +2,14 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const GradContent = ({ blok }) => {
     return (
-        <div className="xl:px-20 flex flex-col gap-2 xs:gap-4 md:gap-8 lg:gap-6 xl:gap-20 items-center lg:grid lg:grid-cols-2 lg:items-center" {...storyblokEditable(blok)}>
-            <img
-                className="h-[200px] w-[380px] sm:h-[250px] lg:h-[300px] w-[430px] lg:w-[500px] lg:order-first lg:justify-self-end rounded md:shadow-2xl"
-                src={blok?.image.filename}
-            />
+        <div className="flex flex-col gap-2 xs:gap-4 md:gap-8 lg:gap-6 xl:gap-20 items-center lg:grid lg:grid-cols-2 lg:items-center" {...storyblokEditable(blok)}>
+            <div className="relative aspect-[7/4] w-full max-w-[520px] lg:order-last lg:justify-self-end rounded-lg md:shadow-2xl">
+                <img
+                    className="absolute h-full w-full rounded-lg"
+                    src={blok?.image.filename}
+                />
+                <div className="absolute h-full w-full bg-gradient-to-bl from-[#001E44] to-65% rounded-lg"></div>
+            </div>
             <div className="flex flex-col gap-2 md:gap-4 lg:max-w-[500px] md:gap-6 text-lg">
                 <p className="px-1 text-base xs:text-lg font-robotoSlab">
                     {blok?.description}
