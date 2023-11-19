db49a5d67ed4ab7c9cddd81a395b9f143fb2acff
Christian Callahan <callahan.0019@gmail.com>
2023-11-18 20:14:51 -0500

    modify CSS

--- components/Content/GradUndergrad/GradUndergradTemplate.js
+++ components/Content/GradUndergrad/GradUndergradTemplate.js
@@ -5,8 +5,7 @@ const GradUndergradTemplate = ({ blok }) => {
         <div
             {...storyblokEditable(blok)}
             style={{ backgroundImage: `url(${blok?.background?.filename})` }}
-            className="bg-cover lg:h-[725px]"
-        >
+            className="bg-cover lg:h-[725px]">
             <div className="h-full py-4 flex flex-col gap-3 justify-center items-center text-center font-semibold sm:px-2 sm:gap-4 md:gap-8 lg:px-5">
                 <h1 className="text-2xl text-blue-950 font-robotoSlab xs:text-4xl">
                     {blok?.header}
