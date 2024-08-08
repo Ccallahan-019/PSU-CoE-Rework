b13d51e4da47c7e7e2f1b0dd6abd5406095c274f
Christian Callahan <callahan.0019@gmail.com>
2024-08-08 16:09:52 -0400

    add gtm to homepage

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ const PAGE_QUERY = gql`
 
 export default async function Home() {
   const slug = "home";
-  
+
   const client = getClient(); //Initialize Apollo Client
   const { loading, error, data } = await client.query({query: PAGE_QUERY, variables: {id: slug}}); //Request data from Storyblok using page query and 'slug' variable
 
@@ -28,6 +28,15 @@ export default async function Home() {
   if (error) return `Error! ${error.message}`;
 
   return (
-    <StoryblokStory story={data?.PageItem} /> //Render page content
+    <div>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QQL6PBRF35"></script>
+      <script>
+        {window.dataLayer = window.dataLayer || []}
+        {function gtag(){dataLayer.push(arguments)}}
+        {gtag('js', new Date())}
+        {gtag('config', 'G-QQL6PBRF35')}
+      </script>
+      <StoryblokStory story={data?.PageItem} />
+    </div>
   );
 };
